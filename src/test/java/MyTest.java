97481bdb3b72c2514b82e4656346f163bedce7dd
papai0709 <jay.papai007@gmail.com>
2025-03-19 08:43:29 +0530

    Added length test

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,6 +3,9 @@ import com.microsoft.demo.Pipeline_Test;
 import org.junit.Test;
 
 public class MyTest {
+
+    Pipeline_Test pt = new Pipeline_Test();
+    String s ="ABCDEFG";
     @Test
     public void test_method_1() {
         Demo d = new Demo();
@@ -15,11 +18,16 @@ public class MyTest {
 
     @Test
     public void test_method_3(){
-        Pipeline_Test pt = new Pipeline_Test();
-        String s ="ABCDEFG";
         String rev = pt.MyTestPipeline(s);
         if (rev.length()==7)
             System.out.println("Test Passed on Length");
 
+    }
+
+    @Test
+    public void test_check_length(){
+        String rev = pt.MyTestPipeline(s);
+        if (rev.length()<9)
+            System.out.println("Length Not Matching..!");
     }
 }
